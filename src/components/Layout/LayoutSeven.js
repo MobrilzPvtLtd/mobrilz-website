c88befc21107a232ba902d797930a85bf6725680
aryannMobrilz <158017892+aryannMobrilz@users.noreply.github.com>
2024-11-21 19:19:41 +0530

    deploy fix

--- src/components/Layout/LayoutSeven.js
+++ src/components/Layout/LayoutSeven.js
@@ -1,12 +1,12 @@
 'use client'
 
 import FooterOne from "../Footer/FooterOne";
-import HeaderSeven from "../Header/HeaderSeven";
+// import HeaderSeven from "../Header/HeaderSeven";
 
 export default function LayoutSeven(props) {
   return (
     <>
-      <HeaderSeven classname={'-style-2 -style-7'} {...props} />
+      {/* <HeaderSeven classname={'-style-2 -style-7'} {...props} /> */}
       {props.children}
       <FooterOne classname={'style-three style-four'} />
     </>
