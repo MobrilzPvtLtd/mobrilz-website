cbabac8d03b311a187458d80f62380f69cd929ee
aryannMobrilz <158017892+aryannMobrilz@users.noreply.github.com>
2024-11-25 17:55:11 +0530

    top bar update

--- src/components/Header/TopNav/TopNavOne.js
+++ src/components/Header/TopNav/TopNavOne.js
@@ -19,37 +19,37 @@ export default function TopNavOne() {
     <>
       <div className="bg-blue-600 text-black font-semibold px-3 py-0 bg-white dark:bg-black dark:text-white flex items-center justify-center text-center">
         <div className="flex justify-center">
-          <div className="hidden lg_2:flex items-center gap-2  px-2.5 py-1 rounded-full ">
+          <div className="hidden lg_2:flex items-center gap-2  px-2.5 py-0 rounded-full ">
             <span className="icon bg-white text-blue w-8 h-8 flex items-center justify-center rounded-full">
-              <Icon.EnvelopeSimple className="text-2xl flex-shrink-0" />
+              <Icon.EnvelopeSimple className=" flex-shrink-0" />
             </span>
             <a
               href="mailto:sales@mobrilz.com"
-              className="text-button text-black whitespace-nowrap flex-shrink-0"
+              className=" text-xs text-black whitespace-nowrap flex-shrink-0"
             >
-              sales@mobrilz.com
+              Sales@Mobrilz.com
             </a>
           </div>
-          <div className="hidden lg_2:flex items-center gap-2  px-2.5 py-[5px] rounded-full ">
+          <div className="hidden lg_2:flex items-center gap-2  px-2.5 py-0 rounded-full ">
             <span className="icon bg-white text-blue w-8 h-8 flex items-center justify-center rounded-full">
-              <Icon.PhoneCall className="text-2xl flex-shrink-0" />
+              <Icon.PhoneCall className=" flex-shrink-0" />
             </span>
             <a
               href="tel:+12568542032"
-              className="text-button text-black whitespace-nowrap flex-shrink-0"
+              className="text-button text-xs text-black whitespace-nowrap flex-shrink-0"
             >
               +1 (256) 854-2032
             </a>
           </div>
-          <div className="hidden lg_2:flex items-center gap-2  px-2.5 py-[5px] rounded-full ">
+          <div className="hidden lg_2:flex items-center gap-2  px-2.5 py-0 rounded-full ">
             <span className="icon bg-white text-blue w-8 h-8 flex items-center justify-center rounded-full">
-              <Icon.MapPin className="text-2xl flex-shrink-0" />
+              <Icon.MapPin className=" flex-shrink-0" />
             </span>
             <a
               href="https://maps.app.goo.gl/iVn8iNN8brrRZYue9"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-button text-black whitespace-nowrap flex-shrink-0"
+              className=" text-xs text-black whitespace-nowrap flex-shrink-0"
             >
               E-38, 2nd Floor, Sector 63 Noida, U.P. 201301, India
             </a>
