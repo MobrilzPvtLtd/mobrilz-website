f223e6eab02da8b10bff21eb7c5429b88724da30
aryannMobrilz <158017892+aryannMobrilz@users.noreply.github.com>
2025-01-14 18:43:30 +0530

    top nav fixed

--- src/components/Header/HeaderOne.js
+++ src/components/Header/HeaderOne.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React ,{ useState, useEffect }  from "react";
 import MenuOne from "./Menu/MenuOne";
 import TopNavOne from "./TopNav/TopNavOne";
 import MenuFive from "./Menu/MenuFive";
 
 export default function HeaderOne(props) {
+  const [hideTopNav, setHideTopNav] = useState(false);
+
+  useEffect(() => {
+    let lastScrollY = window.scrollY;
+
+    const handleScroll = () => {
+      if (window.scrollY > lastScrollY) {
+        setHideTopNav(true); // Hide on scroll down
+      } else {
+        setHideTopNav(false); // Show on scroll up
+      }
+      lastScrollY = window.scrollY;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-    
-    <TopNavOne />
+      {!hideTopNav && <TopNavOne />}
       <MenuOne {...props} />
     </>
   );
